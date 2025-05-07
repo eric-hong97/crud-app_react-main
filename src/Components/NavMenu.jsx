817bd8f0db219dfce27ecd465b3890a33bd2e4ef
eric-hong97 <EricHong0930@outlook.com>
2025-05-07 10:28:20 -0700

    task1

--- src/Components/NavMenu.jsx
+++ src/Components/NavMenu.jsx
@@ -9,7 +9,7 @@ const NavMenu = () => {
     <div>
         <Navbar bg="primary" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home" >Student Management System</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">Student Management System</Navbar.Brand>
           <Nav className="me-auto">
           <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/resultPage">Results</Nav.Link>
